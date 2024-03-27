18ff4cdac56a97c634dbdd25a4c702ef1af52442
amritapandey438 <amritapandey438@gmail.com>
2024-03-27 21:14:59 +0545

    practice project

---
+++ AmritaPandey/conditional_expression.py/IN and IS.py
@@ -0,0 +1,14 @@
+# marks=30
+# if(marks is 30):
+#     print("yes")
+# else:
+#     print("no")
+
+
+
+
+# a=[1,2,3,4,5,0,7]
+# if(2 in a):
+#     print("present")
+# else:
+#     print("not present")    
---
+++ AmritaPandey/conditional_expression.py/logical operators.py
@@ -0,0 +1,25 @@
+# age=45
+# if(age>20 and age<50):
+#     print("you can")
+# else:
+#     print("you can't")    
+
+
+
+
+# age=int(input("Enter your age:"))
+# if(age>30 or age<60):
+#     print("you are adult")
+# else:
+#     print("you aren't")
+
+
+
+
+# age=int(input("Enter your age:"))
+# if(age!>30 or age<60):
+#     print("you are adult")
+# else:
+#     print("you aren't")
+#     pass
+
